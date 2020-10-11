97bffbaaae9fcc5a4b197ed4127815f41db22940
Todd Hossack <todd@tiraki.com>
2020-10-11 15:43:02 +1300

    Added customised getRedirectResponse method to PurchaseResponse which sets header 'Referrer-Policy' to 'no-referrer-when-downgrade' to avoid error with Moneris HPP if using full path in referrer URL security config.

--- src/Message/PurchaseResponse.php
+++ src/Message/PurchaseResponse.php
@@ -3,6 +3,10 @@ namespace Omnipay\Moneris\Message;
 
 use Omnipay\Common\Message\AbstractResponse;
 use Omnipay\Common\Message\RedirectResponseInterface;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse as HttpRedirectResponse;
+
+use RuntimeException;
 
 /**
  * Moneris Purchase Response
@@ -32,5 +36,57 @@ class PurchaseResponse extends AbstractResponse implements RedirectResponseInter
     public function getRedirectData()
     {
         return $this->data;
+    }
+    
+    /**
+     * Slightly customised version of parent method to allow setting of Referrer-Policy with meta tag
+     * @refer \Omnipay\Common\Message\AbstractResponse::getRedirectResponse()
+     * @return HttpRedirectResponse
+     */
+    public function getRedirectResponse()
+    {
+        if (!$this instanceof RedirectResponseInterface || !$this->isRedirect()) {
+            throw new RuntimeException('This response does not support redirection.');
+        }
+
+        if ('GET' === $this->getRedirectMethod()) {
+            return HttpRedirectResponse::create($this->getRedirectUrl());
+        } elseif ('POST' === $this->getRedirectMethod()) {
+            $hiddenFields = '';
+            foreach ($this->getRedirectData() as $key => $value) {
+                $hiddenFields .= sprintf(
+                    '<input type="hidden" name="%1$s" value="%2$s" />',
+                    htmlentities($key, ENT_QUOTES, 'UTF-8', false),
+                    htmlentities($value, ENT_QUOTES, 'UTF-8', false)
+                )."\n";
+            }
+
+            $output = '<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="referrer" content="no-referrer-when-downgrade">
+        <title>Redirecting...</title>
+    </head>
+    <body onload="document.forms[0].submit();">
+        <form action="%1$s" method="post">
+            <p>Redirecting to payment page...</p>
+            <p>
+                %2$s
+                <input type="submit" value="Continue" />
+            </p>
+        </form>
+    </body>
+</html>';
+            $output = sprintf(
+                $output,
+                htmlentities($this->getRedirectUrl(), ENT_QUOTES, 'UTF-8', false),
+                $hiddenFields
+            );
+
+            return HttpResponse::create($output);
+        }
+
+        throw new RuntimeException('Invalid redirect method "'.$this->getRedirectMethod().'".');
     }
 }
